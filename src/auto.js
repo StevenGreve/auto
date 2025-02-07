9e13bd4ad8831915b53b03e6d83fa0afb5cf8668
StevenGreve <steven.bryan.greve@disroot.org>
2025-02-07 12:31:43 +0100

    Save current progress

--- src/auto.js
+++ src/auto.js
@@ -4,13 +4,15 @@ import {
     initTerminal
 } from "./utils.js";
 
+import stringLength from "string-length";
 
 initTerminal();
 mutableStdout.muted = true;
 
 const configs = {
     autoKey: "tab",
-    confirmKey: "return"
+    confirmKey: "return",
+    marker: ">"
 }
 
 // Props
@@ -23,13 +25,25 @@ const blockedKeys = new Set(
 );
 
 let cPos = 0;
-
+let input = "";
+let pCount = 0;
 
 function render(input) {
-
+    process.stdout.write("\x1B[?25l");
+    process.stdout.clearLine(0);
+    process.stdout.write(`\r${configs.marker} ${input}`);
+    process.stdout.cursorTo(stringLength(configs.marker) + 1 + cPos);
+    process.stdout.write("\x1B[?25h");
 }
 
+// Use Cases
+
+// Minimalist Mode
+const model = new Array(5);
+
 function auto() {
+    render(input);
+
     terminal.input.on("keypress", function handler(str, key) {
         if (blockedKeys.has(key.name)) {
             return;
@@ -39,7 +53,38 @@ function auto() {
             // TODO
         } else if (key.ctrl) {
             process.exit();
+        } else if (key.name === "left") {
+            if (cPos > 0) {
+                cPos--;
+            }
+        } else if (key.name === "right") {
+            if (cPos < input.length) {
+                cPos++;
+            }
+        } else if (key.name === "home") {
+            cPos = 0;
+        } else if (key.name === "end") {
+            cPos = input.length;
+        } else if (key.name === "backspace") {
+            if (cPos > 0) {
+                input = input.slice(0, cPos - 1) + input.slice(cPos);
+                cPos--;
+            }
+        } else if (key.name === configs.confirmKey) {
+            terminal.input.removeListener("keypress", handler);
+            pCount++;
+
+            if (pCount < model.length) {
+                auto();
+            } else {
+                process.exit();
+            }
+        } else {
+            input = input.slice(0, cPos) + str + input.slice(cPos);
+            cPos++;
         }
+
+        render(input);
     });
 }
 
